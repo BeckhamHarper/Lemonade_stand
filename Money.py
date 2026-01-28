7635d0cec233681e3b1f4adc7dbd303140d2929f
BeckhamHarper <bh536706@georgetownisd.org>
2026-01-28 10:56:50 -0600

    Adding money.py file.

---
+++ Money.py
@@ -0,0 +1,74 @@
+money= 0
+earnings = 0
+purchases = 0
+lemons = 0
+sugar = 0
+ice_cups = 0
+priceForIce_cups = 2
+difficulty = input("What difficulty would you like to do, Easy, Normal, Hard or Alejandro? ")
+
+def money_setter(difficulty):
+    print("Hello welcome to the Lemonade Stand")
+    global money
+    if difficulty == "Easy":
+        money += 450
+        print("You have "+ str(money) + " dollars")
+    elif difficulty == "Normal":
+        money += 300
+        print("You have "+ str(money) + " dollars")
+    elif difficulty == "Hard":
+        money += 150
+        print("You have "+ str(money) + " dollars")
+    elif difficulty == "Alejandro":
+        money += 1000
+        print("You have "+ str(money) + " dollars")
+    else:
+        print("Ending game")
+        exit()
+
+money_setter(difficulty)
+
+def mainQuestion():
+    global starting_question
+    starting_question = input("Would you like to start day, go to shop, set the price, or check ingredients? ")
+
+def shopQuestions(starting_question):
+    global lemons, sugar, ice_cups, money
+    if starting_question == "go to shop":
+        items = input("Would you like to buy lemons, sugar, or ice_cups? ")
+    
+        if items == "lemons":
+            bought_lemons = input("1$ for 5 lemons, how many do you want to buy? ")
+            priceForLemons = bought_lemons
+            money -= int(bought_lemons)
+            if money < 0:
+                money += int(bought_lemons)
+                print("Not enough money.")
+            elif money > 0:
+                lemons += int(bought_lemons)
+                print("You now have", money, "dollars and ", lemons, " lemons? ")
+    
+        if items == "sugar":
+            bought_sugar = input("1$ for 100 tsp of sugar, how many pounds do you want to buy")
+            priceForSugar = bought_sugar
+            money -= int(bought_sugar)
+            if money < 0:
+                money += int(bought_sugar)
+                print("Not enough money.")
+            elif money > 0:
+                sugar += int(bought_sugar)
+                print("You now have", money, "dollars and ", sugar, " sugar")
+    
+        if items == "ice_cups":
+            bought_ice_cups = int(input("2$ for 200 ice cubes, how many bags do you want to buy"))
+            priceForIce_cups = bought_ice_cups*2
+            money -= int(priceForIce_cups)
+            if money < 0:
+                money += int(bought_ice_cups*2)
+                print("Not enough money.")
+            elif money > 0:
+                
+                ice_cups +=int(bought_ice_cups)
+                print("You now have", money, "dollars and ", ice_cups, " ice cubes")
+current_choice = mainQuestion()
+shopQuestions(starting_question)
