d580db21188b2b4150e79ddf0c76c4afe2b02b52
BeckhamHarper <bh536706@georgetownisd.org>
2026-02-03 20:26:42 -0600

    Add files via upload

---
+++ main_game.py
@@ -0,0 +1,34 @@
+import money
+import recipe
+from customer_class import Customer
+
+
+while True:
+    user_input = input("Type 'stop' to end the game or press Enter to continue: ")
+    if user_input.lower() == "stop":
+        print("Game ended.")
+        break
+
+    money.mainQuestion()
+    if money.starting_question == "go to shop":
+        money.shopQuestions(money.starting_question)
+    elif money.starting_question == "set the price":
+        recipe.cup_price()
+    elif money.starting_question == "change the recipe":
+        recipe.recipe_changer()
+    elif money.starting_question == "check ingredients":
+        print(f"You have {money.money} dollars, {money.lemons} lemons, {money.sugar} tsps of sugar, {money.ice_cups} ice cubes.")
+        print(f"Your recipe: {recipe.recipe['lemons for recipe']} lemons, {recipe.recipe['sugar for recipe']} tsps sugar, {recipe.recipe['ice_cups for recipe']} ice cubes.")
+        print(f"Price per cup: {recipe.price_cup}")
+    elif money.starting_question == "start day":
+        recipe.number_cups()
+    else:
+        print("Invalid option. Please try again.")
+    recipe.number_cups()
+
+customers = []
+
+for i in range(10):
+    customers.append(Customer())
+
+attributes = customers.get_customer_attributes()
