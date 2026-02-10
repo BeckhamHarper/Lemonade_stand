2288506957e470a0144fb9e0b594d84e96f19d3f
BeckhamHarper <bh536706@georgetownisd.org>
2026-02-10 11:13:33 -0600

    Made input easier.

--- Money.py
+++ Money.py
@@ -5,39 +5,50 @@ lemons = 0
 sugar = 0
 ice_cups = 0
 priceForIce_cups = 2
-difficulty = input("What difficulty would you like to do, Easy, Normal, Hard or Alejandro? ")
+difficulty = input("What difficulty would you like to do, Easy(1), Normal(2), Hard(3) or Alejandro(4? ")
+
+
+
 
 def money_setter(difficulty):
-    print("Choose a vaild mode!")
     global money
-    if difficulty == "Easy":
+    if difficulty == "Easy" or "1":
         money += 450
         print("You have "+ str(money) + " dollars")
-    elif difficulty == "Normal":
+    elif difficulty == "Normal" or "2":
         money += 300
         print("You have "+ str(money) + " dollars")
-    elif difficulty == "Hard":
+    elif difficulty == "Hard" or "3":
         money += 150
         print("You have "+ str(money) + " dollars")
-    elif difficulty == "Alejandro":
+    elif difficulty == "Alejandro"or "4":
         money += 1000
         print("You have "+ str(money) + " dollars")
     else:
         print("Ending game")
         exit()
 
+
+
+
 money_setter(difficulty)
+
+
+
 
 def mainQuestion():
     global starting_question
-    starting_question = input("Would you like to start day, go to shop, set the price, change the recipe, or check ingredients? ")
+    starting_question = input("Would you like to start day(1), go to shop(2), set the price(3), change the recipe(4), or check ingredients(5)? ")
+
+
+
 
 def shopQuestions(starting_question):
     global lemons, sugar, ice_cups, money
-    if starting_question == "go to shop":
-        items = input("Would you like to buy lemons, sugar, or ice_cups? ")
-    
-        if items == "lemons":
+    if starting_question == "go to shop" or "2":
+        items = input("Would you like to buy lemons(1), sugar(2), or ice_cups(3)? ")
+   
+        if items == "lemons" or "1":
             bought_lemons = input("1$ for 5 lemons, how many do you want to buy? ")
             priceForLemons = bought_lemons
             money -= int(bought_lemons)
@@ -47,8 +58,8 @@ def shopQuestions(starting_question):
             elif money > 0:
                 lemons += int(bought_lemons)*5
                 print("You now have", money, "dollars and ", lemons, " lemons? ")
-    
-        if items == "sugar":
+   
+        if items == "sugar" or "2":
             bought_sugar = input("1$ for 100 tsp of sugar, how many pounds do you want to buy")
             priceForSugar = bought_sugar
             money -= int(bought_sugar)
@@ -58,9 +69,9 @@ def shopQuestions(starting_question):
             elif money > 0:
                 sugar += int(bought_sugar)*100
                 print("You now have", money, "dollars and ", sugar, " sugar")
-    
-        if items == "ice_cups":
-            priceForIce_cups = bought_ice_cups*2
+   
+        if items == "ice_cups" or "3":
+            bought_ice_cups = int(input("1$ for 100 ice cubes, how many bags do you want to buy"))
             priceForIce_cups = (bought_ice_cups)
             money -= int(priceForIce_cups)
             if money < 0:
@@ -70,6 +81,11 @@ def shopQuestions(starting_question):
                 ice_cups +=int(bought_ice_cups)*100
                 print("You now have", money, "dollars and ", ice_cups, " ice cubes")
 
+
+
+
 current_choice = mainQuestion()
 shopQuestions(starting_question)
+
+
 
