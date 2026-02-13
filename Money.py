c64c73a537661aa7348667a7714177df5ad8d635
BeckhamHarper <bh536706@georgetownisd.org>
2026-02-12 20:06:02 -0600

    Getting rid of some stuff

--- Money.py
+++ Money.py
@@ -5,23 +5,23 @@ lemons = 0
 sugar = 0
 ice_cups = 0
 priceForIce_cups = 2
-difficulty = input("What difficulty would you like to do, Easy(1), Normal(2), Hard(3) or Alejandro(4? ")
+difficulty = input("What difficulty would you like to do, Easy, Normal, Hard or Alejandro? ")
 
 
 
 
 def money_setter(difficulty):
     global money
-    if difficulty == "Easy" or "1":
+    if difficulty == "Easy":
         money += 450
         print("You have "+ str(money) + " dollars")
-    elif difficulty == "Normal" or "2":
+    elif difficulty == "Normal":
         money += 300
         print("You have "+ str(money) + " dollars")
-    elif difficulty == "Hard" or "3":
+    elif difficulty == "Hard":
         money += 150
         print("You have "+ str(money) + " dollars")
-    elif difficulty == "Alejandro"or "4":
+    elif difficulty == "Alejandro":
         money += 1000
         print("You have "+ str(money) + " dollars")
     else:
@@ -38,17 +38,17 @@ money_setter(difficulty)
 
 def mainQuestion():
     global starting_question
-    starting_question = input("Would you like to start day(1), go to shop(2), set the price(3), change the recipe(4), or check ingredients(5)? ")
+    starting_question = input("Would you like to start day, go to shop, set the price, change the recipe, or check ingredients? ")
 
 
 
 
 def shopQuestions(starting_question):
     global lemons, sugar, ice_cups, money
-    if starting_question == "go to shop" or "2":
-        items = input("Would you like to buy lemons(1), sugar(2), or ice_cups(3)? ")
+    if starting_question == "go to shop":
+        items = input("Would you like to buy lemons, sugar, or ice_cups? ")
    
-        if items == "lemons" or "1":
+        if items == "lemons":
             bought_lemons = input("1$ for 5 lemons, how many do you want to buy? ")
             priceForLemons = bought_lemons
             money -= int(bought_lemons)
@@ -59,7 +59,7 @@ def shopQuestions(starting_question):
                 lemons += int(bought_lemons)*5
                 print("You now have", money, "dollars and ", lemons, " lemons? ")
    
-        if items == "sugar" or "2":
+        if items == "sugar":
             bought_sugar = input("1$ for 100 tsp of sugar, how many pounds do you want to buy")
             priceForSugar = bought_sugar
             money -= int(bought_sugar)
@@ -70,7 +70,7 @@ def shopQuestions(starting_question):
                 sugar += int(bought_sugar)*100
                 print("You now have", money, "dollars and ", sugar, " sugar")
    
-        if items == "ice_cups" or "3":
+        if items == "ice_cups":
             bought_ice_cups = int(input("1$ for 100 ice cubes, how many bags do you want to buy"))
             priceForIce_cups = (bought_ice_cups)
             money -= int(priceForIce_cups)
--- recipe.py
+++ recipe.py
@@ -17,7 +17,7 @@ def recipe_changer():
         print("You have,", money.lemons, "lemons")
         
         recipe_lemon = input("How many lemons do you want in your lemonade the minimum is 1 lemons max is 4,\n")
-        if int(recipe_lemon) > 4 or int(recipe_lemon) < 1 or int(recipe_lemon) > money.lemons:
+        if int(recipe_lemon) > 4 or int(recipe_lemon) < 1:
             print("Not a valid number of lemons")
             recipe["lemons for recipe"] = recipe_lemon
         else:    
@@ -26,7 +26,7 @@ def recipe_changer():
         
         print("You have,", money.sugar, "tsps of sugar")
         recipe_sugar = input("How many tps of sugar do you want in your lemonade the minimum is 1 lemons max is 4,\n")
-        if int(recipe_sugar) > 4 or int(recipe_sugar) < 1 or int(recipe_sugar) > money.sugar:
+        if int(recipe_sugar) > 4 or int(recipe_sugar) < 1:
             print("Not a valid number of tps for sugar")
             recipe["sugar for recipe"] = recipe_sugar
         else:    
@@ -35,7 +35,7 @@ def recipe_changer():
 
         print("You have,", money.ice_cups, "ice cubes")
         recipe_ice_cups = input("How many ice cubes do you want in your lemonade the minimum is 1 lemons max is 4,\n")
-        if int(recipe_ice_cups) > 4 or int(recipe_ice_cups) < 1 or int(recipe_ice_cups) > money.ice_cups:
+        if int(recipe_ice_cups) > 4 or int(recipe_ice_cups) < 1:
             print("Not a valid number of ice cubes")
             recipe["ice_cups for recipe"] = recipe_ice_cups
         else:    
@@ -49,14 +49,12 @@ def cup_price():
 recipe_changer()
 cup_price()
 
-print("You have", money.money, "dollars,", money.lemons,"lemons,", money.sugar, "tsps of sugar,", money.ice_cups, "ice cubes.\nYoure recipe consists of", recipe["lemons for recipe"], "lemons", recipe["sugar for recipe"], "tsps of sugar, and", recipe["ice_cups for recipe"], "ice cubes.\nYoure price for cups is", price_cup, "dollars.")
+print("You have", money.money, "dollars,", money.lemons,"lemons,", money.sugar, "tsps of sugar,", money.ice_cups, "ice cubes.\nYoure recipe consists of", recipe["lemons for recipe"], "lemons", recipe["sugar for recipe"], "tsps of sugar, and", recipe["ice_cups for recipe"], "ice cubes.\nYoure price for cups is", price_cup, "dollars.\n")
 
 def number_cups():
-    cups_for_lemons = int(ingredients["lemons"])/int(recipe["lemons for recipe"])
-    cups_for_sugar = ingredients["sugar"]/int(recipe["sugar for recipe"])
-    cups_for_ice_cups = ingredients["ice_cups"]/int(recipe["ice_cups for recipe"])
+    cups_for_lemons = ingredients["lemons"]/recipe["lemons for recipe"]
+    cups_for_sugar = ingredients["sugar"]/recipe["sugar for recipe"]
+    cups_for_ice_cups = ingredients["ice_cups"]/recipe["ice_cups for recipe"]
     print(cups_for_lemons, cups_for_sugar,cups_for_ice_cups)
     return min(cups_for_lemons,cups_for_ice_cups,cups_for_sugar)
-
-print(number_cups())
-
+number_cups()
