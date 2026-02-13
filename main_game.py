90cae3819a5f20332c7d89297388c18704ca1610
BeckhamHarper <bh536706@georgetownisd.org>
2026-02-13 10:24:40 -0600

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -14,9 +14,7 @@ while True:
     elif money.starting_question == "change the recipe":
         recipe.recipe_changer()
     elif money.starting_question == "check ingredients":
-        print(f"You have {money.money} dollars, {money.lemons} lemons, {money.sugar} tsps of sugar, {money.ice_cups} ice cubes.")
-        print(f"Your recipe: {recipe.recipe['lemons for recipe']} lemons, {recipe.recipe['sugar for recipe']} tsps sugar, {recipe.recipe['ice_cups for recipe']} ice cubes.")
-        print(f"Price per cup: {recipe.price_cup}")
+        print("You have", money.money, "dollars,", money.lemons,"lemons,", money.sugar, "tsps of sugar,", money.ice_cups, "ice cubes.\nYoure recipe consists of", recipe["lemons for recipe"], "lemons", recipe["sugar for recipe"], "tsps of sugar, and", recipe["ice_cups for recipe"], "ice cubes.\nYoure price for cups is", price_cup, "dollars.\n")
     elif money.starting_question == "start day":
         day.startDay
         user_input = input("Type 'stop' to end the game or press Enter to continue: ")
